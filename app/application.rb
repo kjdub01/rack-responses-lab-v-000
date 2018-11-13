1d03edc64facea27b86816ab2759bf027b7770bd
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-11-13 04:40:57 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    resp.write "Hello, World"
+    if time <= 12:00
+    resp.write "Good Mornimg!"
     
     resp.finish
   end
